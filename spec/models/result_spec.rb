d1309f4230bc4568c0c44155dff8c86371cbb621
sjBao <supuilam@gmail.com>
2017-04-26 11:59:02 -0400

    Write create results migration

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Result, type: :model do
   describe "Result" do
-    it "has a winner attribute"
+    it "has a winner"
   end
 
-  it "has many players "
+  it "has many players"
 end
