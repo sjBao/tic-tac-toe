3d6a878705cf7768ee97238eab33b57d72162dfa
sjBao <supuilam@gmail.com>
2017-04-25 22:45:35 -0400

    Rename route component directories.
    Rename directories to follow fb conventions.
    Add boiler plate code to sub components Score and
    NotFound page.

---
+++ client/src/pages/Score/Score.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Score extends Component {
+  render() {
+    return (
+      <h1>Score Page</h1>
+    )
+  }
+}
+
+export default Score;
---
+++ client/src/routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Router, Route} from 'react-router';
+
+import App from './App';
+import Game from ' ./pages/Game/Game';
+import Score from './pages/Score/Score';
+import NotFound from './pages/NotFound/NotFound'
