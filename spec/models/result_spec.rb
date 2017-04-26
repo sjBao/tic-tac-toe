a3bfc4348b2d36053420c1656dd8b16b6e2506d0
sjBao <supuilam@gmail.com>
2017-04-26 18:31:40 -0400

    Add more description to game_spec.rb

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Result, type: :model do
-  describe "Result" do
+RSpec.describe Game, type: :model do
+  describe "Game" do
     it "has a winner"
+
+    it "has a player1"
+
+    it "has a player2"
   end
 
   it "has many players"
