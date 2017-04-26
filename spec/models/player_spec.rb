cc49279fe3d791c4b85add720d98a357e812fed6
sjBao <supuilam@gmail.com>
2017-04-26 10:26:32 -0400

    Add basic validation for player name

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "when name is empty" do
+    let(:player) { Player.new }
+
+    it "should not be valid" do
+      expect(player.valid?).to be_false
+    end
+
+    it "should not save" do
+      expect(user.save).to be_false
+    end
+  end
 end
