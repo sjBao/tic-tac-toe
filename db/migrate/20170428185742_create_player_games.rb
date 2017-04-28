26befa0d5e2b1be6df95f6176cd9ab161722f9e9
sjBao <supuilam@gmail.com>
2017-04-28 14:59:49 -0400

    Write migration for player_games

--- db/migrate/20170428185742_create_player_games.rb
+++ db/migrate/20170428185742_create_player_games.rb
@@ -1,6 +1,9 @@
 class CreatePlayerGames < ActiveRecord::Migration[5.0]
   def change
     create_table :player_games do |t|
+      t.integer :game_id
+      t.integer :player1
+      t.integer :player2
 
       t.timestamps
     end
