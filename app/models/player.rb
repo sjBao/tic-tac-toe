6a1b25482ca7d7231041deb97e8bc17ce9ad298f
sjBao <supuilam@gmail.com>
2017-04-28 15:10:52 -0400

    Pass games_played test for Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,8 @@ class Player < ApplicationRecord
   validates :name, presence: true
   has_many :games_won,
     :class_name => Game, :foreign_key => 'winner_id'
+
+  has_many :player_game
+  has_many :games_played,
+    :through => :player_game, :source => :game
 end
