960b90e45e298051f78f0d27009fdc358bd0ef89
sjBao <supuilam@gmail.com>
2017-04-28 15:05:24 -0400

    Pass belongs to tests for PlayerGame

--- app/models/player_game.rb
+++ app/models/player_game.rb
@@ -1,2 +1,9 @@
 class PlayerGame < ApplicationRecord
+  belongs_to :player1,
+    class_name: Player, foreign_key: 'player1'
+
+  belongs_to :player2,
+    class_name: Player, foreign_key: 'player2'
+
+  belongs_to :game
 end
