48358d7df18dd88483f65cb16cc0a179a4075b68
sjBao <supuilam@gmail.com>
2017-04-28 14:21:06 -0400

    Add player migration

---
+++ db/migrate/20170428181942_create_player.rb
@@ -0,0 +1,11 @@
+class CreatePlayer < ActiveRecord::Migration[5.0]
+  def change
+    create_table :players do |t|
+      t.string :name, null: false
+
+      t.timestamps(null: false)
+    end
+
+    add_index: :name, unique: true
+  end
+end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,4 +1,3 @@
 require 'rails_helper'
-
 RSpec.describe Player, type: :model do
 end
