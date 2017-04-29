d691ec10f575b16bb1f3bbf5c632f2ef51d1b695
sjBao <supuilam@gmail.com>
2017-04-28 20:52:13 -0400

    Write and pass test for #match association for Games.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   belongs_to :player, foreign_key: 'winner_id'
+  has_many :matches, :class_name => PlayerGame
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   context "associations" do
     it { should belong_to(:player) }
+    it { should have_many(:matches) }
   end
 end
