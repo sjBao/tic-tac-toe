51ab8fa1ed6e162b99d53bff173cd5f67116410a
sjBao <supuilam@gmail.com>
2017-04-29 16:34:40 -0400

    Add more tests for Games.
    Game with no winner should return "Draw",
    Game with no winner should still be valid,
    Game with no winner should still save.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Game, type: :model do
     it { should belong_to(:winner) }
     it { should have_one(:matches) }
   end
+
+  context "When there is no winner" do
+    let(:game) { Game.new }
+
+    it "should return 'Draw'" do
+      expect(game).to eq('Draw')
+    end
+
+    it "should still be valid" do
+      expect(game.valid?).to be true
+    end
+
+    it "should still save" do
+      expect(game.save).to be true
+    end
+  end
 end
