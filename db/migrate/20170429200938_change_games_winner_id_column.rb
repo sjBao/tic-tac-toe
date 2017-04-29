e7bf57ff4eaa02696918a46d1902c5a0c681fcf6
sjBao <supuilam@gmail.com>
2017-04-29 16:12:10 -0400

    Run change games column migration

--- db/migrate/20170429200938_change_games_winner_id_column.rb
+++ db/migrate/20170429200938_change_games_winner_id_column.rb
@@ -1,5 +1,5 @@
 class ChangeGamesWinnerIdColumn < ActiveRecord::Migration[5.0]
   def change
-    change_column :games, :winner_id, null: true
+    change_column :games, :winner_id, :integer, null: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428185742) do
+ActiveRecord::Schema.define(version: 20170429200938) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "winner_id"
