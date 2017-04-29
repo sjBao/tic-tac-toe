8240763cb3c8a1d7d0082a36404744206d6976fa
sjBao <supuilam@gmail.com>
2017-04-29 16:14:02 -0400

    Make winner_id column optional

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ApplicationRecord
-  belongs_to :player, foreign_key: 'winner_id'
+  belongs_to :player, foreign_key: 'winner_id', optional: true
   has_one :matches, :class_name => PlayerGame
 end
