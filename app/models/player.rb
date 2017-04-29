9960ccb875a060653ed002bae04e3202d827bee9
sjBao <supuilam@gmail.com>
2017-04-28 20:58:53 -0400

    Change association of games to matches to has_one

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,6 +4,6 @@ class Player < ApplicationRecord
     :class_name => Game, :foreign_key => 'winner_id'
 
   has_many :matches, :class_name => PlayerGame
-  has_many :games_played,
+  has_one :games_played,
     :through => :matches, :source => :game
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  context "associations" do
+  context "Game Associations" do
     it { should belong_to(:player) }
     it { should have_many(:matches) }
   end
