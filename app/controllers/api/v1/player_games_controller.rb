43a66f0ba1e9f227fa8fb0ed9581d152e26230e4
sjBao <supuilam@gmail.com>
2017-04-29 22:56:14 -0400

    Create post route PlayerGames#create

--- app/controllers/api/v1/player_games_controller.rb
+++ app/controllers/api/v1/player_games_controller.rb
@@ -7,6 +7,21 @@ class Api::V1::PlayerGamesController < ApplicationController
     @matches = PlayerGame.order(:created_at => 'DESC').limit(5)
     render :index
   end
+
+  def create
+    @match = PlayerGame.new
+    @match.player1 = Player.find_or_create_by(name: params[:player1])
+    @match.player1 = Player.find_or_create_by(name: params[:player2])
+    if params[:winner]
+      @match.game = Game.create(Player.find_or_create_by(name: params[:winner]))
+    end
+    @match.game = Game.create
+  end
+
+  private
+
+  def player_game_params
+  end
 end
 
 
