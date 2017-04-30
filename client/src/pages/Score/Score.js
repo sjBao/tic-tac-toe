5721367f78de85a7c5f9471705ab2c5284c84f79
sjBao <supuilam@gmail.com>
2017-04-29 23:57:06 -0400

    Score page shows most recent matches

--- client/src/pages/Score/Score.js
+++ client/src/pages/Score/Score.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
 
 class Score extends Component {
+  state = {
+    recentMatches: [],
+    leaderboard: [],
+  };
+
+  componentWillMount = () => {
+    fetch('/v1/recent_matches', {
+      accept: 'application/json'
+    }).then(promise => {
+      return promise.json()
+    }).then(response => {
+      this.setState({recentMatches: response});
+    });
+  }
+
+
   render() {
+    let { recentMatches, leaderboard } = this.state
     return (
-      <h1>Score Page</h1>
+      <div className="match-history">
+        <h1>Score Page</h1>
+
+        <div className="recentMatches">
+          <table>
+            <thead>
+              <tr>
+                <th>Player1</th>
+                <th>Player2</th>
+                <th>Winner</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                recentMatches.map((match, i) => {
+                  return(
+                    <tr key={i}>
+                      <td>{ match.player1.name }</td>
+                      <td>{ match.player2.name }</td>
+                      <td>{ match.winner }</td>
+                      <td>{ match.created_at }</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
     )
   }
 }
