dbc1f41673828a69647da04e6d08a5933b041c4d
sjBao <supuilam@gmail.com>
2017-05-01 16:20:13 -0400

    Abstract fetching from API into methods

--- client/src/pages/Score/Leaderboard.js
+++ client/src/pages/Score/Leaderboard.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import dateFormat from 'dateformat';
 import './Leaderboard.css'
 
 const Leaderboard = (props) => {
--- client/src/pages/Score/Score.js
+++ client/src/pages/Score/Score.js
@@ -8,22 +8,29 @@ class Score extends Component {
     leaderboard: [],
   };
 
-  componentWillMount = () => {
-    fetch('/v1/recent_matches', {
+  fetchRecentMatches= () => {
+    fetch(`/v1/recent_matches`, {
       accept: 'application/json'
     }).then(promise => {
       return promise.json()
     }).then(response => {
       this.setState({recentMatches: response});
     });
+  };
 
-    fetch('/v1/most_wins', {
+  fetchLeaderboard= () => {
+    fetch(`/v1/most_wins`, {
       accept: 'application/json'
     }).then(promise => {
       return promise.json()
     }).then(response => {
-      this.setState( {leaderboard: response} );
+      this.setState({leaderboard: response});
     });
+  };
+
+  componentWillMount = () => {
+    this.fetchRecentMatches();
+    this.fetchLeaderboard();
   }
 
   render() {
