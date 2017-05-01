97ce7493983628850b45c176b72e07f8fad74ef7
sjBao <supuilam@gmail.com>
2017-05-01 14:46:33 -0400

    Extract Recent matches to its own component

---
+++ client/src/pages/Score/RecentMatches.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const RecentMatches = (props) => {
+  return (
+    <div className="recentMatches">
+      <table>
+        <thead>
+          <tr>
+            <th>Player1</th>
+            <th>Player2</th>
+            <th>Winner</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.recentMatches.map((match, i) => {
+              return(
+                <tr key={i}>
+                  <td>{ match.player1.name }</td>
+                  <td>{ match.player2.name }</td>
+                  <td>{ match.winner }</td>
+                  <td>{ match.created_at }</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default RecentMatches;
--- client/src/pages/Score/Score.js
+++ client/src/pages/Score/Score.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RecentMatches from './RecentMatches';
 
 class Score extends Component {
   state = {
@@ -22,33 +23,7 @@ class Score extends Component {
     return (
       <div className="match-history">
         <h1>Score Page</h1>
-
-        <div className="recentMatches">
-          <table>
-            <thead>
-              <tr>
-                <th>Player1</th>
-                <th>Player2</th>
-                <th>Winner</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                recentMatches.map((match, i) => {
-                  return(
-                    <tr key={i}>
-                      <td>{ match.player1.name }</td>
-                      <td>{ match.player2.name }</td>
-                      <td>{ match.winner }</td>
-                      <td>{ match.created_at }</td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table>
-        </div>
+        <RecentMatches recentMatches={recentMatches} />
       </div>
     )
   }
