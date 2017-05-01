51db4f74c6e61c13dc12dec655f6d04c476d4611
sjBao <supuilam@gmail.com>
2017-05-01 17:24:21 -0400

    Create form to save match results.
    Game Menu will now be able to send POST request
    to PlayerGames#create.

--- client/src/pages/game/GameMenu.js
+++ client/src/pages/game/GameMenu.js
@@ -4,7 +4,24 @@ class GameMenu extends Component {
   state = {
     submittingResults: false,
   }
-  
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.refs);
+    fetch("/v1/player_games", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        player1: 'Bandy Biufu',
+        player2: 'Kebin Bhen',
+        winner: ''
+      })
+    })
+  }
+
   render() {
     let { reset, winner, gameOver } = this.props
 
@@ -12,6 +29,15 @@ class GameMenu extends Component {
       <div className={"game-menu "+ gameOver}>
         { gameOver ? <NewGameBtn reset={reset} /> : '' }
         { gameOver && winner ? <SubmitResultsBtn /> : '' }
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="player1">Player1 name:</label>
+          <input id="player1" type="text" name="player1" ref="player1"/>
+          <br/>
+          <br/>
+          <label htmlFor="player2">Player2 name:</label>
+          <input id="player2" type="text" name="player2" ref="player2"/>
+          <input type="submit"/>
+        </form>
       </div>
     )
   };
