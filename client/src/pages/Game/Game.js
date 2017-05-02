3fb44da19d6c196665d01602e769520d0bdd07cc
sjBao <supuilam@gmail.com>
2017-05-02 14:00:23 -0400

    modify package.json for deployment

--- client/src/pages/Game/Game.js
+++ client/src/pages/Game/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import BoardBox from './BoardBox';
+import BoardBox from './BoardBox.js';
 import GameMenu from './GameMenu';
 import GameStatus from './GameStatus';
 import './Game.css';
